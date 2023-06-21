fbf65145354e3a15828df35959c20833e5870682
Dmitry Potepalov <dmitry.potepalov@aiven.io>
2023-06-21 11:20:07 +0200

    Add replace_address_first_boot to yaml config
    It's more convenient to set it there in Astacus. Option semantics is
    preserved, the value specified in Java properties has priority.

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -931,7 +931,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     private boolean isReplacing()
     {
-        if (System.getProperty("cassandra.replace_address_first_boot", null) != null && SystemKeyspace.bootstrapComplete())
+        if (DatabaseDescriptor.replaceOnFirstBootRequested() && SystemKeyspace.bootstrapComplete())
         {
             logger.info("Replace address on first boot requested; this node is already bootstrapped");
             return false;
