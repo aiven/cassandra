1f3138796a184be25a3f3e7bad6b8eb890d6483c
Denis Potapov <denis.potapov@aiven.io>
2023-10-12 15:02:22 +0200

    Optionally skip streaming when replacing nodes
    The data will be provided by Astacus, streaming from other nodes is not
    necessary.

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2060,6 +2060,13 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         invalidateLocalRanges();
         repairPaxosForTopologyChange("bootstrap");
 
+        if (DatabaseDescriptor.skipBootstrapStreaming())
+        {
+            bootstrapFinished();
+            logger.info("Bootstrap skipped for tokens {}", tokens);
+            return true;
+        }
+
         Future<StreamState> bootstrapStream = startBootstrap(tokens);
         try
         {
